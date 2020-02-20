a7a28a3971f398da4b0f239d64b5b222535314b4
Steaton <steaton3@gmail.com>
2020-02-20 18:15:34 +0100

    Added mvc controller tests.

--- src/main/java/com/ing/productDetails/ProductDetailsController.java
+++ src/main/java/com/ing/productDetails/ProductDetailsController.java
@@ -1,5 +1,6 @@
 package com.ing.productDetails;
 
+import com.ing.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
