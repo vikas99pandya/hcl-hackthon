9dec4cbc2dcf22d945e020368ac1bd73880f3b6b
Steaton <steaton3@gmail.com>
2020-02-20 19:34:21 +0100

    Added additional mock mvc tests.

--- src/test/java/com/ing/product/ProductControllerIntegrationTest.java
+++ src/test/java/com/ing/product/ProductControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.ing.product;
 
 import com.ing.UserAccountApp;
+import com.ing.models.Product;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,10 +12,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,7 +38,7 @@ public class ProductControllerIntegrationTest {
 
     @Test
     @Ignore
-    public void should_retrieve_product_groups_with_200() throws Exception {
+    public void should_retrieve_all_product_groups_with_200() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/productGroups")
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -42,11 +48,56 @@ public class ProductControllerIntegrationTest {
 
     @Test
     @Ignore
-    public void should_handle_not_found_product_with_404() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/productGroups")
+    public void should_handle_empty_product_group_with_200() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/productGroups")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        assertEquals("[]", content);
+    }
+
+    @Test
+    @Ignore
+    public void should_handle_not_found_product_group_with_404() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/product/1234")
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isNotFound())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value(anyString()));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description")
+                        .value("error message to be inserted here"));
+    }
+
+    @Test
+    @Ignore
+    public void should_handle_empty_products_list_with_200() throws Exception {
+        when(productService.getProducts(1234)).thenReturn(new ArrayList<>());
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/product/1234")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        assertEquals("[]", content);
+    }
+
+    @Test
+    @Ignore
+    public void should_handle_products_list_with_200() throws Exception {
+        when(productService.getProducts(1234)).thenReturn(Arrays.asList(new Product(1, "name", 1, 1)));
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/product/1234")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        assertEquals("expected goes here", content);
     }
 }
