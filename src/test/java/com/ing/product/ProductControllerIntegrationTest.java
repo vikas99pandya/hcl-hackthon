5061abb1f2b271fc3446d9c38d8770dbc6ac2b42
Steaton <steaton3@gmail.com>
2020-02-20 18:26:58 +0100

    Added mvc controller tests.

--- src/test/java/com/ing/product/ProductControllerIntegrationTest.java
+++ src/test/java/com/ing/product/ProductControllerIntegrationTest.java
@@ -1,16 +1,10 @@
 package com.ing.product;
 
-import com.ing.UserAccountApp;
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -19,9 +13,8 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(ProductController.class)
-@ContextConfiguration(classes = UserAccountApp.class)
+@Ignore
 public class ProductControllerIntegrationTest {
 
     @Autowired
